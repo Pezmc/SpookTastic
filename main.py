c5e2d2c9a57c4551c9b2765e348338813f3b683b
Pez Cuckow <email@pezcuckow.com>
2019-10-02 22:02:22 +0100

    Refactor to avoid blocking main thread with sleep

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ import time
 import glob
 import random
 import sys
-from omxplayer.player import OMXPlayer
+from omxplayer.player import OMXPlayer, OMXPlayerDeadError
+from dbus import DBusException
 from pathlib import Path
 
+DEV_MODE = True
+FULLSCREEN = False
+
+#### Setup
 
 GPIO.setmode(GPIO.BCM)
 PIR_PIN = 14
@@ -22,19 +27,17 @@ random.shuffle(videosInQueue)
 
 print "Found",  len(videos), "videos"
 
-FULLSCREEN = False
-
 if FULLSCREEN:
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 else:
     screen = pygame.display.set_mode((640, 480))
 
-pygame.display.set_caption("Spook Tastic")
+pygame.display.set_caption("SpookTastic")
 pygame.mouse.set_visible(False)
 
-####
+#### Functions
 
-def play_random_video():
+def start_random_video():
     global videosInQueue
 
     if len(videosInQueue) == 0:
@@ -44,83 +47,111 @@ def play_random_video():
     video = videosInQueue.pop()
 
     print "Playing video", video
-    play_video(video)
+    return play_video(video)
 
 def play_video(video):
     VIDEO_PATH = Path(video)
     print VIDEO_PATH
 
-
     player = None
     try: 
-        player = OMXPlayer(VIDEO_PATH)
+        player = OMXPlayer(VIDEO_PATH, args='--no-osd -o alsa')
 
         # App is real slow to boot and start playing video
-        time.sleep(5)
+        time.sleep(1)
 
         player.play()
-
-        while player.is_playing():
-            print "status:", player.playback_status(),
-            print "position:", player.position()
-            time.sleep(0.1)
 
     except SystemError:
         print "OMXPlayer failed to start, maybe"
         print "Sleeping for 60 seconds in case it didn't actually fail..."
-        print player
         time.sleep(60)
 
     except OMXPlayerDeadError:
         print "OMXPlayer appeared to close, video likely ended!"
-        print "Waiting 15 seconds"
-        time.sleep(15)
 
-    finally: 
-        sleepFor = random.randint(15,60)
-        print "Played video, not playing another for", sleepFor, "seconds"
-        time.sleep(sleepFor)
+    except DBusException:
+        print "OMXPlayer not replying, video likely ended!"
 
+    finally:
+        return player
 
-######
+def check_if_video_playing(player):
+    try: 
+        if not player:
+            return False
+
+        return player.is_playing()
+
+    except OMXPlayerDeadError:
+        print "OMXPlayer appeared to close, video likely ended!"
+        return False
+
+    except DBusException:
+        print "OMXPlayer not replying, video likely ended!"
+        return False
+
+def check_events():
+    sys.stdout.write(".")
+    sys.stdout.flush()
+
+    global FULLSCREEN
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print "Pygame quit triggered, closing"
+            pygame.quit()
+            return False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE or event.unicode.lower() == 'q':
+                print "Escape pressed, closing"
+                pygame.quit()
+                return False
+            elif event.unicode.lower() == "f":
+                print "Toggling fullscreen"
+                FULLSCREEN = not FULLSCREEN
+                if FULLSCREEN:
+                    pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+                else:
+                    pygame.display.set_mode((640, 480))
+
+    return True
+
+#### Main loop
 
 pygame.init()
+running = True
+
+videoPlaying = False
+videoPlayer = None
+
+waitUntilBeforeNextVideo = time.time()
+
 print "Ready"
 try:
-    running = True
-    while running:
-        sys.stdout.write(".")
-        sys.stdout.flush()
-        for event in pygame.event.get():
-            #print "Event", event
-            if event.type == pygame.QUIT:
-                running = False
-                break
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE or event.unicode.lower() == 'q':
-                    print "Escape pressed, closing"
-                    running = False
-                    break
-                elif event.unicode.lower() == "f":
-                    print "Toggling fullscreen"
-                    FULLSCREEN = not FULLSCREEN
-                    if FULLSCREEN:
-                        pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-                    else:
-                        pygame.display.set_mode((640, 480))
+    while running:        
+        running = check_events()
+        if not running: 
+            if videoPlayer:
+                videoPlayer.quit()
+            break
 
+        if videoPlaying:
+            videoPlaying = check_if_video_playing(videoPlayer)
+            if not videoPlaying:
+                sleepFor = random.randint(1 if DEV_MODE else 15, 5 if DEV_MODE else 45)
+                waitUntilBeforeNextVideo = time.time() + sleepFor
+                print "Played video, not playing another for", sleepFor, "seconds"
 
-        if GPIO.input(PIR_PIN):
-            print "Motion Detected!"
-            play_random_video()
+        else:
+            if GPIO.input(PIR_PIN):
+                print "Motion Detected!"
+                if time.time() > waitUntilBeforeNextVideo:
+                    videoPlaying = True
+                    videoPlayer = start_random_video()
+                else:
+                    print "Not triggering video, waiting until", waitUntilBeforeNextVideo
 
         time.sleep(0.25)
 
 finally:
     pygame.quit()
-
-# Close screen
-pygame.quit()
-
-#######
-
